ba4d3d07d92d93826d5abcebbdb18300490e82d7
RITika2522 <ritikachandak2502@gmail.com>
2026-01-05 21:53:39 +0530

    Question 35 practiced

---
+++ ques35.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1, 3, 5, 6};
+    int target = 2;
+
+    int index = sol.searchInsert(nums, target);
+
+    cout << "Insert position: " << index << endl;
+
+    return 0;
+}
